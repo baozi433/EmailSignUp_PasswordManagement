6a541a9ded7d8ec9b4de945249c0da095a89fd91
Yuanjie Wu <yuanjiewu925@gmail.com>
2022-06-02 14:24:05 +1200

    Set up user and database 1.Add User model 2.Installed EntityFramework Core, design, and sqlserver package. 3.Setup the database

---
+++ EmailSignUp_PasswordManagement/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmailSignUp_PasswordManagement.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        //Connection string can also be set in the appsetting.json file
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.
+                UseSqlServer("Server=.\\SQLEXPRESS;Database=userdb;Trusted_Connection=True;");
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
--- EmailSignUp_PasswordManagement/Program.cs
+++ EmailSignUp_PasswordManagement/Program.cs
@@ -1,3 +1,6 @@
+global using EmailSignUp_PasswordManagement.Models;
+global using EmailSignUp_PasswordManagement.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
 
